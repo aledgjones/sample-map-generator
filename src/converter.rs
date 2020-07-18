4d6b5ade18485b969309dd7393862ae89cf49c07
Aled Jones <aledjones.viola@gmail.com>
2020-07-18 23:00:24 +0100

    added normalizing

--- src/converter.rs
+++ src/converter.rs
@@ -2,20 +2,21 @@ use crate::progress::{next, start};
 use crate::utils::get_pitch;
 use crate::utils::{clear_directory, delete_directory};
 use base64::encode;
+use regex::Regex;
 use std::collections::HashMap;
 use std::env;
 use std::fs::read;
 use std::fs::read_dir;
-use std::io::Result;
 use std::path::PathBuf;
 use std::process::Command;
+use std::result::Result;
 
 pub fn get_samples(
     source_dir: &PathBuf,
     instrument: &str,
     expression: &str,
-) -> Result<HashMap<String, String>> {
-    let root_dir = env::current_dir()?;
+) -> Result<HashMap<String, String>, String> {
+    let root_dir = env::current_dir().unwrap();
     let temp_dir = root_dir.join("temp");
 
     clear_directory(&temp_dir);
@@ -27,8 +28,8 @@ pub fn get_samples(
 
     start(&instrument, &expression);
 
-    for entry in read_dir(source_dir)? {
-        let entry = entry?;
+    for entry in read_dir(source_dir).unwrap() {
+        let entry = entry.unwrap();
         let path = entry.path();
         let path = path.as_path();
         let metadata = entry.metadata().unwrap();
@@ -36,15 +37,44 @@ pub fn get_samples(
         if metadata.is_file() == true && path.extension().unwrap() == "wav" {
             let src = path.to_str().unwrap();
             let pitch = get_pitch(&src).unwrap();
-            let dest = temp_dir.join(format!("{}.mp3", pitch));
+            let dest = temp_dir.join(format!("{}.ogg", pitch));
             let dest = dest.as_path();
             let dest = dest.to_str().unwrap();
 
-            let result = Command::new("lame")
-                .arg("-v")
-                .args(&["-b", "8"])
-                .args(&["-B", "64"])
-                .arg(src)
+            // https://trac.ffmpeg.org/wiki/AudioVolume
+
+            // measure peaks -- first pass
+            let result = Command::new("ffmpeg")
+                .args(&["-i", src])
+                .args(&["-af", "volumedetect", "-f", "null", "-"])
+                .output();
+
+            let diff = match result {
+                Ok(log) => {
+                    let pattern = Regex::new(r"max_volume: (.*) dB").unwrap();
+                    let output = String::from_utf8(log.stderr).unwrap();
+                    let level = match pattern.captures(&output) {
+                        Some(cap) => match cap.get(1) {
+                            Some(t) => t.as_str(),
+                            None => "0.0",
+                        },
+                        None => "0.0",
+                    };
+                    let level: f64 = level.parse().unwrap();
+                    let diff = -6.0 - level;
+                    format!("{:.1}dB", diff)
+                }
+                Err(_) => {
+                    println!("Error converting. Is Lame installed?");
+                    continue;
+                }
+            };
+
+            // apply filter with calulated params -- second pass
+            let filter = format!("volume={}", diff);
+            let result = Command::new("ffmpeg")
+                .args(&["-i", src])
+                .args(&["-af", &filter])
                 .arg(dest)
                 .output();
 
@@ -58,7 +88,7 @@ pub fn get_samples(
 
             let data = read(dest).unwrap();
             let data = encode(&data);
-            samples.insert(pitch, format!("data:audo/mpeg;base64,{}", data));
+            samples.insert(pitch, format!("data:audo/ogg;base64,{}", data));
 
             i = i + 1.0;
             next(&instrument, &expression, &len, &i);
